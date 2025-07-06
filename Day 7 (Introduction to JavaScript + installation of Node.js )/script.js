9d765e255432e0add4808f47e850f9cf38ab5eb9
mirzasahab90 <124375075+mirzasahab90@users.noreply.github.com>
2025-07-06 20:49:51 +0500

    123

---
+++ Day 7 (Introduction to JavaScript + installation of Node.js )/script.js
@@ -0,0 +1,21 @@
+//Data type 
+/*	1. integer (0-9)
+	2. float (0.0-99.9)
+	3. double (0.0-99.9)
+	4. boolean (1/0)
+	5. char (a-z)
+	6. string (sentences)  */
+
+    // var a = 2;
+    // const b = 3 ;
+    // var c = a + b ;
+    // console.log(c)
+
+    // var a = 10; // number 
+    // var b = "Rayyan" // string 
+    // var c = [1, 2, 3, 4, 5] // array 
+    // console.log(a);
+    // console.log(b);
+    // console.log(c[2]);
+
+    
