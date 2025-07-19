fdcac895cf372dd1dfb6eb3eaea090947d93494a
mirzasahab90 <124375075+mirzasahab90@users.noreply.github.com>
2025-07-19 20:16:57 +0500

    new

--- Day 7 (Introduction to JavaScript + installation of Node.js )/script.js
+++ Day 7 (Introduction to JavaScript + installation of Node.js )/script.js
@@ -1,21 +1,34 @@
 //Data type 
 /*	1. integer (0-9)
-	2. float (0.0-99.9)
-	3. double (0.0-99.9)
-	4. boolean (1/0)
-	5. char (a-z)
-	6. string (sentences)  */
+    2. float (0.0-99.9)
+    3. double (0.0-99.9)
+    4. boolean (1/0)
+    5. char (a-z)
+    6. string (sentences)  */
 
-    // var a = 2;
-    // const b = 3 ;
-    // var c = a + b ;
-    // console.log(c)
+// var a = 2;
+// const b = 3 ;
+// var c = a + b ;
+// console.log(c)
 
-    // var a = 10; // number 
-    // var b = "Rayyan" // string 
-    // var c = [1, 2, 3, 4, 5] // array 
-    // console.log(a);
-    // console.log(b);
-    // console.log(c[2]);
+// var a = 10; // number 
+// var b = "Rayyan" // string 
+// var c = [1, 2, 3, 4, 5] // array 
+// console.log(a);
+// console.log(b);
+// console.log(c[2]);
 
-    
+let a = [1, 2, 3, 4, 5, 6];
+let b = [2, 7, 8, 3, 2, 6];
+
+// Step 1: Find duplicates between a and b
+let duplicates = a.filter(value => b.includes(value));
+
+// Step 2: Filter out duplicates from both arrays
+let uniqueA = a.filter(value => !duplicates.includes(value));
+let uniqueB = b.filter(value => !duplicates.includes(value));
+
+// Step 3: Combine the filtered arrays and sum all values
+let total = [...uniqueA, ...uniqueB].reduce((sum, val) => sum + val, 0);
+
+console.log("Sum of non-duplicate values:", total);
---
+++ Day 8/script.js
@@ -0,0 +1,53 @@
+// let a = 'Hello world'
+// console.log (a);
+
+
+
+// let obj = {name:"Rayyan", age : "18", class: "1st Year" }
+
+// console.log(obj.class)
+
+
+
+// let b = 21 ;
+
+// if ( b % 2 ){
+// console.log ("b has modulus with 2");
+// }
+// else if ( b % 3 ){
+// console.log ("b has modulus with 3")
+// }
+// else if ( b % 4 ){
+// console.log ("b has modulus with 4")
+// }
+// else {
+//     console.log("no modulus")
+// }
+
+// var c = 0;
+// let x = 11 ;
+// const e = 0;
+
+
+// if (x > 10) {
+//     let y = 20;
+//   console.log(y); // 20
+// }
+// console.log(y); // ReferenceError: y is not defined
+
+
+
+// let x = "banana";
+// switch (x) {
+//   case "apple":
+//     console.log("x is an apple");
+//     break;
+//   case "banana":
+//     console.log("x is a banana");
+//     break;
+//   case "orange":
+//     console.log("x is an orange");
+//     break;
+//   default:
+//     console.log("x is something else");
+// }
