bebf58ea3c9236448aef14b1b011a7aebb7b9c9d
mirzasahab90 <124375075+mirzasahab90@users.noreply.github.com>
2025-07-20 20:52:32 +0500

    new

---
+++ Day 9/script.js
@@ -0,0 +1,40 @@
+// for (initialization; condition; increment / decrement) {
+//     // code to be executed
+// }
+
+// let f = 7.8;
+
+// let i;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// let person = {
+//   name: "John",
+//   age: 30,
+//   job: "developer"
+// };
+
+// console.log(person.job)
+
+// for (let i in person) {
+//   console.log(i + ": " + person[i]);
+// }
+
+
+// let numbers = [1, 2, 3, 4, 5];
+ 
+// // console.log(numbers[0])
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
+let i = 1;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+
