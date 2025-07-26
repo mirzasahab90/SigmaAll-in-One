80407321fd6aedb33a315bf12fca596c7dc581aa
mirzasahab90 <124375075+mirzasahab90@users.noreply.github.com>
2025-07-26 20:26:37 +0500

    new

---
+++ Day 10/script.js
@@ -0,0 +1,49 @@
+// console.log("Hello World")
+
+// ==============================
+// var a = "Hello World";
+// console.log(a.length);
+
+// ==============================
+// var str1 = "Hello";
+// var str2 = " World";
+// console.log(str1.concat(str2));
+
+// ==============================
+
+// let a = [2,3,4,5,6]
+// console.log(a)
+
+// var str = "Hello World";
+// console.log(str.indexOf("W"))
+
+// ===============================
+
+// var t = "Hello World";
+// console.log(t.slice(4))
+
+// ===============================
+
+// var str = "Hello World";
+// console.log(str.replace("World", "Universe"));
+
+// ===============================
+// var str = "HellO wOrld"
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+
+// ===============================
+
+// var myArray = [1, "Hello", [2, 3]];
+// myArray.splice(1, 1, "Hello World", [4, 5]);
+// console.log(myArray); 
+
+// ===============================
+
+// var myArray = [1, "Hello", [2, 3]];
+// console.log(myArray.slice(0, 2)); 
+
+// var myArray = [1, 2, 3, 4, 5];
+// for (var i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i]);
+// }
