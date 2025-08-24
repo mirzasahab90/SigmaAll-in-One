502eb2455bbdb11051306426e37009be47b2714f
mirzasahab90 <124375075+mirzasahab90@users.noreply.github.com>
2025-08-24 20:04:19 +0500

    Day 16

---
+++ Day 16 (Introduction to React)/my-first-app/src/module1.mjs
@@ -0,0 +1,6 @@
+import q ,{a,b,d }from "./module2.mjs";
+
+console.log(a);
+console.log(b);
+console.log(q);
+
---
+++ Day 16 (Introduction to React)/my-first-app/src/module2.mjs
@@ -0,0 +1,8 @@
+const a = "Zeeshan";
+const b = "Rayyan";
+const c = "Uzair";
+const d = "Noman";
+
+export default c ;
+export {a,b,d} ;
+
